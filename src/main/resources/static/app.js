5c2c7135cf9941eb8d09c129eba35e4f4e69f2cd
crisado <crisado@DESKTOP-4LP8F1R>
2023-04-20 13:56:07 +0200

    remove unused code

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,14 +1,8 @@
 import { createApp } from 'vue'
 import router from './Route.js'
 import FormatterPlugin from "./plugin/FormatterPlugin.js";
-import axios from 'axios';
 
 flatpickr.l10ns.default.firstDayOfWeek = 1;
-axios
-	.get('/test')
-	.then(response => {
-		console.log(response.data)
-	})
 
 const app = createApp({})
 app.use(router)
